a6ce4cab450acd08429c079f5e25ee97e202dd4c
ShaneB <117615586+shanerbrewer@users.noreply.github.com>
2023-10-15 14:57:38 -0500

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+import openai
+
+
+openai.api_key = "insert OpenAI API key here"
+
+
+
+def chat(role,prompt):
+  textstuff = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": role},
+        {"role": "user", "content": prompt}
+    ]
+  )
+  return(textstuff.choices[0].message.content)
+
+
+
+
+chat_qs = ["What is an axolotl?",
+           "What is an amphibian", 
+           "What is hello world?"]
+
+chat_roles = ["You are an expert on amphibians.", "You are an expert on programming"]
+outputs = []
+
+for x in chat_roles:
+  for i in chat_qs:
+    outputs.append(chat(x,i))
+
+for p in outputs:
+  print(p)
