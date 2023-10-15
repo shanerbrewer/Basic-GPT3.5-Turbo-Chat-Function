42dd89601b955785d68ceaf913a120f6d33ae950
ShaneB <117615586+shanerbrewer@users.noreply.github.com>
2023-10-15 15:10:34 -0500

    Update chatbot.py
    updated variable names for clarity

--- chatbot.py
+++ chatbot.py
@@ -3,31 +3,27 @@ import openai
 
 openai.api_key = "insert OpenAI API key here"
 
-
-
 def chat(role,prompt):
-  textstuff = openai.ChatCompletion.create(
+  chatcompletion = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": role},
         {"role": "user", "content": prompt}
     ]
   )
-  return(textstuff.choices[0].message.content)
+  return(chatcompletion.choices[0].message.content)
 
-
-
-
-chat_qs = ["What is an axolotl?",
+chat_inputs = ["What is an axolotl?",
            "What is an amphibian", 
           ]
 
-chat_roles = ["You are an expert on amphibians."]
-outputs = []
+chat_role = "You are an expert on amphibians."
+responses = []
 
-for x in chat_roles:
-  for i in chat_qs:
-    outputs.append(chat(x,i))
 
-for p in outputs:
-  print(p)
+for chat_input in chat_inputs:
+  responses.append(chat(chat_role,chat_input))
+
+for response in responses:
+  print(response)
+
